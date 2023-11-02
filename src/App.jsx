a95d065901885855856e73b80fc621426bfd8a5c
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2023-11-01 20:42:32 -0500

    finishing CP2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Video from 'twilio-video';
-import './App.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import Video from "twilio-video";
+import "./App.css";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const [roomName, setRoomName] = useState('');
+  const [roomName, setRoomName] = useState("");
   const [room, setRoom] = useState(null);
   const containerRef = useRef(null);
   const navigate = useNavigate();
 
-  const handleDisconnectedParticipant = React.useCallback((participant) => {
-    participant.removeAllListeners();
-    const participantDiv = document.getElementById(participant.identity);
-    if (participantDiv) participantDiv.remove();
-    if (room.participants.size === 1) {
-      const localContainer = document.getElementById(room.localParticipant.identity);
-      if (localContainer) {
+  const handleDisconnectedParticipant = React.useCallback(
+    (participant) => {
+      participant.removeAllListeners();
+      const participantDiv = document.getElementById(participant.identity);
+      if (participantDiv) participantDiv.remove();
+      if (room.participants.size === 1) {
+        const localContainer = document.getElementById(
+          room.localParticipant.identity,
+        );
+        if (localContainer) {
           localContainer.classList.remove("two-participants");
+        }
       }
-    }
-  }, [room]);
+    },
+    [room],
+  );
 
   useEffect(() => {
     if (room) {
@@ -29,13 +34,13 @@ function App() {
         participantDiv.classList.add("participant");
         containerRef.current.appendChild(participantDiv);
         // containerRef.current.appendChild(participantDiv);
-        
+
         participant.tracks.forEach((trackPublication) =>
-          handleTrackPublication(trackPublication, participant)
+          handleTrackPublication(trackPublication, participant),
         );
-        
+
         participant.on("trackPublished", (trackPublication) =>
-          handleTrackPublication(trackPublication, participant)
+          handleTrackPublication(trackPublication, participant),
         );
       };
 
@@ -43,7 +48,7 @@ function App() {
       room.participants.forEach(handleConnectedParticipant);
       room.on("participantConnected", handleConnectedParticipant);
       room.on("participantDisconnected", handleDisconnectedParticipant);
-      
+
       return () => {
         window.removeEventListener("pagehide", room.disconnect);
         window.removeEventListener("beforeunload", room.disconnect);
@@ -57,12 +62,10 @@ function App() {
       participantDiv.appendChild(trackPublication.track.attach());
     }
 
-    trackPublication.on("subscribed", track => {
+    trackPublication.on("subscribed", (track) => {
       participantDiv.appendChild(track.attach());
     });
   };
-
-
 
   const joinVideoRoom = async (roomName, token) => {
     const room = await Video.connect(token, { room: roomName });
@@ -74,7 +77,7 @@ function App() {
     const response = await fetch("https://lang-server.onrender.com/join-room", {
       method: "POST",
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ roomName }),
@@ -83,53 +86,53 @@ function App() {
     try {
       await joinVideoRoom(roomName, token);
       navigate(`/room/${roomName}`);
-    }
-    catch (err) {
+    } catch (err) {
       alert(err);
     }
   };
 
-const handleDisconnect = () => {
+  const handleDisconnect = () => {
     room.disconnect();
     setRoom(null);
     navigate("/");
     setRoomName("");
-};
+  };
 
-
-return (
-  <div>
-    {!room ? (
-      
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={roomName}
-          onChange={(e) => setRoomName(e.target.value)}
-        />
-        <button type="submit">Join Room</button>
-        <p className="instructions">
-          MVP: <br></br>Enter a room name and hit the Join Room button. 
-          Switch to a different browser (or, send a link and the room name to friend) and use the same URL and enter the same room name, 
-          for example: Room1, and join the room. You would see two screens with two videos streaming at the same time. 
-          There is a disconnect button for you to disconnect from the live video streaming.
-          <br></br>
-          There could only be at max two participants in a room, since we want to make it like Omegle.
-        </p>
-      </form>
-      
-    ) : (
-      <div>
-        <div ref={containerRef} className='container'></div>
-        <div className="translucent-banner">
-          <button className="disconnect-button" onClick={handleDisconnect}>
-            Disconnect
-          </button>
+  return (
+    <div>
+      {!room ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter room name"
+            value={roomName}
+            onChange={(e) => setRoomName(e.target.value)}
+          />
+          <button type="submit">Join Room</button>
+          <p className="instructions">
+            <h4><b>MVP</b></h4>
+            <ul>
+              <li><strong>Please wait for a few seconds for the server to wake up after clicking the Join Room once.</strong></li>
+              <li>Enter a room name you want to create/join, and hit the Join Room button. </li>
+              <li>Switch to a different browser (or, send a link and the room name to a friend) and use the same<br></br> URL and enter the same room name, 
+                for example: Room1, and join the room. You would see <br></br>two screens with two videos streaming at the same time.</li>
+              <li>There is a disconnect button for you to disconnect from the live video streaming.</li>
+              <li>There could only be at max two participants in a room, since we want to make it like Omegle.</li>
+            </ul>
+          </p>
+        </form>
+      ) : (
+        <div>
+          <div ref={containerRef} className="container"></div>
+          <div className="translucent-banner">
+            <button className="disconnect-button" onClick={handleDisconnect}>
+              Disconnect
+            </button>
+          </div>
         </div>
-      </div>
-    )}
-  </div>
-);
+      )}
+    </div>
+  );
 }
 
 export default App;
