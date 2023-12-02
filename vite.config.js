6776bcbf4120ad56eef072d91d6f13d20a5e90bb
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2023-12-01 22:48:19 -0600

    build configuration

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    aalias: {
-      '/src/worklets': new URL('src/worklets', import.meta.url).pathname,
-    },
-    extensions: ['.js'], // Add any other extensions that your modules might use
+  build: {
+    sourcemap: true,
   },
 });
